156ba250b3dd2f4839aa422578e1c7199697aa2a
Connor Davis <cnnrproton@gmail.com>
2023-09-26 10:56:44 +0200

    Fix bug for vsa security products where it should return skipped for failed updates

--- src/routes/vsa/security_products.rs
+++ src/routes/vsa/security_products.rs
@@ -25,32 +25,40 @@ pub async fn import(State(pool): State<PgPool>) -> impl IntoResponse {
     }
 
     let mut updated = 0;
+    let mut skipped = 0;
 
     for valid_product in valid_antivirus_products {
-        let product_agent = sqlx::query_as!(
+        let product_agent_result = sqlx::query_as!(
             VsaAgent,
             "SELECT * FROM vsa_agents WHERE id = $1;",
             valid_product.agent_id
         )
         .fetch_one(&pool)
-        .await
-        .expect("Failed to find vsa product agent from postgres.");
+        .await;
 
-        sqlx::query_as!(
-            VsaAgent,
-            "UPDATE vsa_agents SET anti_virus = $1 WHERE id = $2",
-            true,
-            product_agent.id
-        )
-        .execute(&pool)
-        .await
-        .expect("Failed to update vsa agent in postgres database.");
+        match product_agent_result {
+            Ok(product_agent) => {
+                sqlx::query_as!(
+                    VsaAgent,
+                    "UPDATE vsa_agents SET anti_virus = $1 WHERE id = $2",
+                    true,
+                    product_agent.id
+                )
+                .execute(&pool)
+                .await
+                .expect("Failed to update vsa agent in postgres database.");
 
-        updated += 1;
+                updated += 1;
+            }
+            Err(_) => {
+                skipped += 1;
+            }
+        }
     }
 
     Json(json!({
         "status": StatusCode::OK.as_u16(),
-        "updated": updated
+        "updated": updated,
+        "skipped": skipped
     }))
 }
